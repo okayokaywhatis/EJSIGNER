8837659728e88f476a8d189a04ff2bc644a0fda1
okayokaywhatis <theskyscraperscausingvertigo@gmail.com>
2026-01-31 01:04:43 -0800

    Create IPASignerModule.h for iOS app signing
    Added header file for the IPASignerModule interface.

---
+++ ios/IPASignerModule.h
@@ -0,0 +1,13 @@
+//
+//  IPASignerModule.h
+//  SignerApp
+//
+//  Advanced iOS App Signing Module
+//
+
+#import <React/RCTBridgeModule.h>
+#import <Foundation/Foundation.h>
+
+@interface IPASignerModule : NSObject <RCTBridgeModule>
+
+@end
